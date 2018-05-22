3411f8f7a231856ca0fd6f9c213f0a1f2fdbceab
cx52018761 <cx52018761@hotmail.com>
2018-05-22 15:09:28 +0800

    properties修改

--- shiro-core/src/main/java/com/zyd/shiro/framework/holder/RequestHolder.java
+++ shiro-core/src/main/java/com/zyd/shiro/framework/holder/RequestHolder.java
@@ -1,30 +1,7 @@
-/**
- * MIT License
- *
- * Copyright (c) 2018 yadong.zhang
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+
 package com.zyd.shiro.framework.holder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -34,79 +11,66 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- *
  * @author nicky_chin [shuilianpiying@163.com]
-
  * @version 1.0
  * @date 2018/4/16 16:26
  * @since 1.0
  */
+@Slf4j
 public class RequestHolder {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RequestHolder.class);
 
     /**
      * 获取request
-     *
      * @return HttpServletRequest
      */
     public static HttpServletRequest getRequest() {
-        LOGGER.debug("getRequest -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        log.debug("getRequest -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
         return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
     }
 
     /**
      * 获取Response
-     *
      * @return HttpServletRequest
      */
     public static HttpServletResponse getResponse() {
-        LOGGER.debug("getResponse -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        log.debug("getResponse -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
         return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
     }
 
     /**
      * 获取session
-     *
      * @return HttpSession
      */
     public static HttpSession getSession() {
-        LOGGER.debug("getSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        log.debug("getSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
         return getRequest().getSession();
     }
 
     /**
      * 获取session的Attribute
-     *
      * @param name
      *         session的key
      * @return Object
      */
     public static Object getSession(String name) {
-        LOGGER.debug("getSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getAttribute(name, RequestAttributes.SCOPE_SESSION);
+        log.debug("getSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        return RequestContextHolder.getRequestAttributes().getAttribute(name, RequestAttributes.SCOPE_SESSION);
     }
 
     /**
      * 添加session
-     *
-     * @param name
-     * @param value
      */
     public static void setSession(String name, Object value) {
-        LOGGER.debug("setSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-        ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).setAttribute(name, value, RequestAttributes.SCOPE_SESSION);
+        log.debug("setSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        RequestContextHolder.getRequestAttributes().setAttribute(name, value, RequestAttributes.SCOPE_SESSION);
     }
 
     /**
      * 清除指定session
-     *
-     * @param name
-     * @return void
      */
     public static void removeSession(String name) {
-        LOGGER.debug("removeSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-        ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).removeAttribute(name, RequestAttributes.SCOPE_SESSION);
+        log.debug("removeSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        RequestContextHolder.getRequestAttributes().removeAttribute(name, RequestAttributes.SCOPE_SESSION);
     }
 
     /**
@@ -115,7 +79,7 @@ public class RequestHolder {
      * @return String[]
      */
     public static String[] getSessionKeys() {
-        LOGGER.debug("getSessionKeys -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
-        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getAttributeNames(RequestAttributes.SCOPE_SESSION);
+        log.debug("getSessionKeys -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        return RequestContextHolder.getRequestAttributes().getAttributeNames(RequestAttributes.SCOPE_SESSION);
     }
 }
--- shiro-core/src/main/java/com/zyd/shiro/framework/tag/CustomTagDirective.java
+++ shiro-core/src/main/java/com/zyd/shiro/framework/tag/CustomTagDirective.java
@@ -1,26 +1,4 @@
-/**
- * MIT License
- *
- * Copyright (c) 2018 yadong.zhang
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+
 package com.zyd.shiro.framework.tag;
 
 import com.zyd.shiro.business.service.SysResourcesService;
@@ -36,16 +14,16 @@ import java.util.Map;
 
 /**
  * 自定义的freemarker标签
- *
  * @author nicky_chin [shuilianpiying@163.com]
  * @version 1.0
-
  * @date 2018/4/16 16:26
  * @since 1.0
  */
 @Component
 public class CustomTagDirective implements TemplateDirectiveModel {
+
     private static final String METHOD_KEY = "method";
+
     @Autowired
     private SysResourcesService resourcesService;
 
@@ -57,13 +35,14 @@ public class CustomTagDirective implements TemplateDirectiveModel {
             switch (method) {
                 case "availableMenus":
                     // 获取所有可用的菜单资源
-                    environment.setVariable("availableMenus", builder.build().wrap(resourcesService.listAllAvailableMenu()));
+                    environment.setVariable("availableMenus",
+                            builder.build().wrap(resourcesService.listAllAvailableMenu()));
                     break;
                 case "menus":
                     Integer userId = null;
                     if (map.containsKey("userId")) {
                         String userIdStr = map.get("userId").toString();
-                        if(StringUtils.isEmpty(userIdStr)){
+                        if (StringUtils.isEmpty(userIdStr)) {
                             return;
                         }
                         userId = Integer.parseInt(userIdStr);
