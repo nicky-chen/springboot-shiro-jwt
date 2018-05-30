053754f612cbf462140de00516d3afec0aa1d02b
cx52018761 <cx52018761@hotmail.com>
2018-05-30 16:03:44 +0800

    range

--- shiro-admin/src/test/java/com/nicky/shiro/ShiroAdminApplicationTests.java
+++ shiro-admin/src/test/java/com/nicky/shiro/ShiroAdminApplicationTests.java
@@ -2,8 +2,10 @@ package com.nicky.shiro;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Range;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,6 +23,13 @@ public class ShiroAdminApplicationTests {
         List<Integer> list = Lists.newArrayListWithExpectedSize(16);
         Object a = null;
         map.put("error", "7777");
+        List<String> l = ImmutableList.of("a", "b", "c");
+        System.out.println(l);
+        int aa = 1;
+        int bb = 3;
+        int cc = 5;
+        System.out.println(Range.closedOpen(aa, cc).contains(5));
+
         try {
             //Preconditions.checkNotNull(a, map);
             Preconditions.checkArgument(!Strings.isNullOrEmpty(""), "用户名不能为空");
